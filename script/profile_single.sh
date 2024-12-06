58abee0c50fc60955f0d39fdb8804a11399a087e
Yoon Noh Lee <yoonnohlee@yonsei.ac.kr>
2024-12-07 02:08:31 +0900

    Update profile_single.sh
    modfiy script

--- script/profile_single.sh
+++ script/profile_single.sh
@@ -54,9 +54,10 @@ python profiler.py --model=gpt2-medium --batch=8 --seq_len=512 --sm=$sm --precis
 # CUTLASS openllama-3B
 python profiler.py --model=openlm-research/open_llama_3b --batch=1 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS --tmp_dir=$tmp_dir
 python profiler.py --model=openlm-research/open_llama_3b --batch=4 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS --tmp_dir=$tmp_dir
-python profiler.py --model=openlm-research/open_llama_3b --batch=8 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS --tmp_dir=$tmp_dir
 
 if [ "$tmp_dir" = "./cutlass_a6000" ]; then
+
+python profiler.py --model=openlm-research/open_llama_3b --batch=8 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS --tmp_dir=$tmp_dir
 
 python profiler.py --model=meta-llama/Meta-Llama-3-8B-Instruct --batch=1 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS --tmp_dir=$tmp_dir
 python profiler.py --model=meta-llama/Meta-Llama-3-8B-Instruct --batch=4 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS --tmp_dir=$tmp_dir
@@ -114,10 +115,11 @@ python profiler.py --model=gpt2-medium --batch=8 --seq_len=512 --sm=$sm --precis
 # CUTLASS FMHA openllama-3B
 python profiler.py --model=openlm-research/open_llama_3b --batch=1 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS_FMHA --tmp_dir=$tmp_dir
 python profiler.py --model=openlm-research/open_llama_3b --batch=4 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS_FMHA --tmp_dir=$tmp_dir
-python profiler.py --model=openlm-research/open_llama_3b --batch=8 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS_FMHA --tmp_dir=$tmp_dir
 
 
 if [ "$tmp_dir" = "./cutlass_a6000" ]; then
+
+python profiler.py --model=openlm-research/open_llama_3b --batch=8 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS_FMHA --tmp_dir=$tmp_dir
 
 python profiler.py --model=meta-llama/Meta-Llama-3-8B-Instruct --batch=1 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS_FMHA --tmp_dir=$tmp_dir
 python profiler.py --model=meta-llama/Meta-Llama-3-8B-Instruct --batch=4 --seq_len=512 --sm=$sm --precision=float32 --target_lib=CUTLASS_FMHA --tmp_dir=$tmp_dir
@@ -180,7 +182,7 @@ if [ "$tmp_dir" = "./cutlass_a6000" ]; then
 
 python profiler.py --model=openlm-research/open_llama_3b --batch=8 --seq_len=512 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
 
-# cuBLAs MetaLlama3-8B
+# # cuBLAs MetaLlama3-8B
 python profiler.py --model=meta-llama/Meta-Llama-3-8B-Instruct --batch=1 --seq_len=512 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
 python profiler.py --model=meta-llama/Meta-Llama-3-8B-Instruct --batch=4 --seq_len=512 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
 python profiler.py --model=meta-llama/Meta-Llama-3-8B-Instruct --batch=8 --seq_len=512 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
