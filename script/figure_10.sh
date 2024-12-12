0fa3cb1185bc6dba20858448d0997d29ed4e9512
yoon5862 <yoonnohlee@yonsei.ac.kr>
2024-12-12 07:24:58 +0000

    modify code

--- script/figure_10.sh
+++ script/figure_10.sh
@@ -24,18 +24,18 @@ python figure_10.py  --batch=12 --sm=$sm --precision=float32 --target_lib=CUTLAS
 python figure_10.py  --batch=16 --sm=$sm --precision=float32 --target_lib=CUTLASS --tmp_dir=$tmp_dir
 
 
-# #cuBLAS Batch = 2
-# python figure_10.py  --batch=2 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
+#cuBLAS Batch = 2
+python figure_10.py  --batch=2 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
 
-# #cuBLAS Batch = 4
-# python figure_10.py  --batch=4 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
+#cuBLAS Batch = 4
+python figure_10.py  --batch=4 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
 
-# #cuBLAS Batch = 8
-# python figure_10.py  --batch=8 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
+#cuBLAS Batch = 8
+python figure_10.py  --batch=8 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
 
-# #cuBLAS Batch = 12
-# python figure_10.py  --batch=12 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
+#cuBLAS Batch = 12
+python figure_10.py  --batch=12 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
 
-# #cuBLAS Batch = 16
-# python figure_10.py  --batch=16 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
+#cuBLAS Batch = 16
+python figure_10.py  --batch=16 --sm=$sm --precision=float32 --target_lib=cuBLAS --tmp_dir=$tmp_dir
 
